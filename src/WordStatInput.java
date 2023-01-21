66cd48fe4fbd9e441639897eaec3ef35dfcf2da8
VMihail <83347281+VMihail@users.noreply.github.com>
2023-01-21 17:12:33 +0300

    WordStatInput

---
+++ src/WordStatInput.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class WordStatInput {
+  public static void main(String[] args) {
+    if (args.length != 2) {
+      throw new InvalidNumberOfArguments(2);
+    }
+    String inputFileName = args[0];
+    String outputFileName = args[1];
+    Scanner in = null;
+    try {
+      in = new Scanner(new File(inputFileName));
+    } catch (FileNotFoundException e) {
+      System.out.println("input file not found " + e.getMessage());
+    }
+    Map<String, Integer> dictionary;
+    try {
+      assert in != null;
+      dictionary =  new LinkedHashMap<>();
+      while (in.hasNextLine()) {
+        String nextLine = in.nextLine().toLowerCase();
+        StringBuilder nextWord = new StringBuilder();
+        for (int i = 0; i < nextLine.length(); i++) {
+          char nextChar = nextLine.charAt(i);
+          if (Character.isLetter(nextChar) || nextChar == '\'' || Character.getType(nextChar) == Character.DASH_PUNCTUATION) {
+            nextWord.append(nextChar);
+          } else if (nextWord.length() > 0) {
+            String word = nextWord.toString();
+            nextWord.setLength(0);
+            if (dictionary.containsKey(word)) {
+              dictionary.put(word, dictionary.get(word) + 1);
+            } else {
+              dictionary.put(word, 1);
+            }
+          }
+        }
+      }
+    } finally {
+      assert in != null;
+      in.close();
+    }
+    PrintWriter out = null;
+    try {
+      out = new PrintWriter(outputFileName);
+    } catch (IOException e) {
+      System.out.println("Output file not found " + e.getMessage());
+    }
+    try {
+      assert out != null;
+      for (Map.Entry<String, Integer> entry : dictionary.entrySet()) {
+        out.println(entry.getKey() + " " + entry.getValue());
+      }
+    } finally {
+      assert out != null;
+      out.close();
+    }
+  }
+
+  static class InvalidNumberOfArguments extends RuntimeException {
+    public InvalidNumberOfArguments(int count) {
+      super("Invalid number of arguments, expected " + count);
+    }
+  }
+}
